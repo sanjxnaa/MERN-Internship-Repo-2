f2112486d5cc38925c3095746cea05162a44013c
Sanjana Gowda <94379989+sanjxnaa@users.noreply.github.com>
2023-10-05 09:02:45 +0530

    Add files via upload

---
+++ js-task7.js
@@ -0,0 +1,7 @@
+function myPlayFunction() {
+    document.getElementById("myDIV").style.animationPlayState = "running";
+  }
+  
+  function myPauseFunction() {
+    document.getElementById("myDIV").style.animationPlayState = "paused";
+  }
